38bccbd328cd636415ada2a7a8c0310a10ca2ea0
Raj Kulkarni <74099005+RajKulk16@users.noreply.github.com>
2023-09-19 13:58:24 +0530

    model scripting

---
+++ script.py
@@ -0,0 +1,97 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, f1_score, precision_score, recall_score
+import sklearn
+import joblib
+import boto3
+import pathlib
+from io import StringIO
+import argparse
+import joblib
+import os
+import numpy as np
+import pandas as pd
+
+# loading the model from the model directory (we will be saving the model  in the S3 bucket)
+def model_fn(model_dir):
+    clf = joblib.load(os.path.join(model_dir, "model.joblib"))
+    return clf
+
+if __name__ == "__main__":
+    
+    print("[INFO] Extracting the Arguments")
+    parser = argparse.ArgumentParser()
+    
+    # hyperparameters
+    parser.add_argument('--n_estimators', type=int, default=100)
+    parser.add_argument('--random_state', type=int, default=0)
+    
+    # Data, model, and output directories
+    parser.add_argument('--model_dir', type=str, default=os.environ.get('SM_MODEL_DIR'))
+    parser.add_argument('--train', type=str, default=os.environ.get('SM_CHANNEL_TRAIN'))
+    parser.add_argument('--test', type=str, default=os.environ.get('SM_CHANNEL_TEST'))
+    parser.add_argument('--train_file', type=str, default='train-V-1.csv')
+    parser.add_argument('--test_file', type=str, default='test-V-1.csv')
+    
+    args, _ = parser.parse_known_args()
+    
+    print('Sklearn Version:', sklearn.__version__)
+    print('Joblib Version:', joblib.__version__)
+    
+    print("[INFO] Reading the data")
+    print()
+    
+    train_df = pd.read_csv(os.path.join(args.train, args.train_file))
+    test_df = pd.read_csv(os.path.join(args.test, args.test_file))
+    
+    features = list(train_df.columns)
+    label = features.pop(-1)
+    
+    print('Building, training and evaluating the model')
+    print()
+    
+    X_train = train_df[features]
+    X_test = test_df[features]
+    y_train = train_df[label]
+    y_test = test_df[label]
+    
+    print('Column Order: ')
+    print(features)
+    print()
+    
+    print('Label Column: ', label)
+    print()
+    
+    print("Data Shape: ")
+    print()
+    print("---- SHAPE OF TRAINING DATA (85%) ----")
+    print(X_train.shape)
+    print(y_train.shape)
+    print()
+    print("---- SHAPE OF TESTING DATA (15%) ----")
+    print(X_test.shape)
+    print(y_test.shape)
+    print()
+    
+    print("Training RandomForest Model.....")
+    print()
+    model =  RandomForestClassifier(n_estimators=args.n_estimators, random_state=args.random_state, verbose = 3,n_jobs=-1)
+    model.fit(X_train, y_train)
+    print()
+    
+    # Dumping the model after training (similar to pickle.dump())
+    model_path = os.path.join(args.model_dir, "model.joblib")
+    joblib.dump(model,model_path)
+    print("Model persisted at " + model_path)
+    print()
+    
+    y_pred_test = model.predict(X_test)
+    test_acc = accuracy_score(y_test,y_pred_test)
+    test_rep = classification_report(y_test,y_pred_test)
+
+    print()
+    print("---- METRICS RESULTS FOR TESTING DATA ----")
+    print()
+    print("Total Rows are: ", X_test.shape[0])
+    print('[TESTING] Model Accuracy is: ', test_acc)
+    print('[TESTING] Testing Report: ')
+    print(test_rep)
